072c33a89910f7a3e57bcc2310e2714f16017bec
Kamyyyk <kamyk962@gmail.com>
2022-11-01 17:10:01 +0100

    Third commit

--- src/app/main-header/main-header.component.ts
+++ src/app/main-header/main-header.component.ts
@@ -7,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainHeaderComponent implements OnInit {
   user: string | undefined
+  opened: boolean | undefined
 
   constructor() { }
+
+  openMenu(): void {
+    this.opened = !this.opened
+  }
 
   ngOnInit(): void {
     this.user = 'Użytkownik testowy'
