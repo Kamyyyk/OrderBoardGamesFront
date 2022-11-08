a7f98960deeb5a5d10140d05846a583f2d9e418e
Kamyyyk <kamyk962@gmail.com>
2022-11-08 20:42:54 +0100

    added notification service

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,21 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import {NotificationService} from "../notification.service";
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
+  providers: [MessageService, NotificationService]
 })
 export class ContactComponent implements OnInit {
-  value1: string | undefined
+  value1: string | undefined;
 
   showValue() {
-    console.log(this.value1)
+    console.log(this.value1);
   }
-  constructor() { }
 
+  showSuccess() {
+    this.notificationService.showSuccess()
+  }
+
+  constructor(private notificationService: NotificationService) {}
 
   ngOnInit(): void {
-    this.showValue()
+    this.showValue();
   }
-
 }
